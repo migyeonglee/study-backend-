a1dea4170e1be37b3425da235e6ded64c4a92482
migyeonglee <ak10044@naver.com>
2022-08-11 16:37:01 +0900

    study: express ejs

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+app.use('/static', express.static('static'));
 //서버 접속 포트
 const port = 3000;
 // 주소 ip:3000/
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+    // res.sendFile(__dirname + "/app.js");
+    //"test"== ejs 파일 이름
+    // res.render("test");
+    var person = [
+        { name: "이미경", gender: "woman" },
+        { name: "홍길동", gender: "man" }
+    ]
+    res.render("test", { per: person });
+});
+app.get("/test", (req, res) => {
+    res.render("test1");
 });
 // 서버를 열겠다 / 동작할때 무언가
 app.listen(port, () => {
