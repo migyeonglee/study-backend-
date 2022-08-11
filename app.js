ac8ddaa5c8d21df9ad50a4e0420227e80c5fdb35
migyeonglee <ak10044@naver.com>
2022-08-11 17:42:37 +0900

    study: express ejs test

--- app.js
+++ app.js
@@ -5,17 +5,31 @@ app.use('/static', express.static('static'));
 //서버 접속 포트
 const port = 3000;
 // 주소 ip:3000/
+// app.get("/", (req, res) => {
+//     // res.sendFile(__dirname + "/app.js");
+//     //"test"== ejs 파일 이름
+//     // res.render("test");
+//     var person = [
+//         { name: "이미경", gender: "woman" },
+//         { name: "홍길동", gender: "man" }
+//     ]
+//     res.render("test", { per: person });
+// });
 app.get("/", (req, res) => {
-    // res.sendFile(__dirname + "/app.js");
-    //"test"== ejs 파일 이름
-    // res.render("test");
-    var person = [
-        { name: "이미경", gender: "woman" },
-        { name: "홍길동", gender: "man" }
-    ]
-    res.render("test", { per: person });
+    res.render("test");
 });
-app.get("/test", (req, res) => {
+app.get("/test0", (req, res) => {
+    var coffee = [
+        { menu: "에스프레소" },
+        { menu: "아메리카노" },
+        { menu: "카페라떼" },
+        { menu: "카푸치노" },
+        { menu: "모카라떼" },
+
+    ]
+    res.render("test0", { cfe: coffee });
+});
+app.get("/test1", (req, res) => {
     res.render("test1");
 });
 // 서버를 열겠다 / 동작할때 무언가
